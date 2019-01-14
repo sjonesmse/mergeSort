2eb6a9f875698345bed362b23de02f42316a23e8
pfgallagher <pfgallagher@gmail.com>
2019-01-14 14:35:07 -0600

    more implementation of merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,27 +1,28 @@
 function split(wholeArray) {
-  /* your code here to define the firstHalf and secondHalf arrays */
+	/* your code here to define the firstHalf and secondHalf arrays */
 
-  let midPoint = Math.floor(wholeArray.length / 2);
-  let firstHalf = wholeArray.slice(0, midPoint);
-  let secondHalf = wholeArray.slice(midPoint);
+	const midPoint = Math.floor(wholeArray.length / 2);
+	const firstHalf = wholeArray.slice(0, midPoint);
+	const secondHalf = wholeArray.slice(midPoint);
 
-  return [firstHalf, secondHalf];
+	return [firstHalf, secondHalf];
 }
 
 function merge(arr1, arr2) {
-  let returnArr = [];
-  let firstIndex = 0;
-  let secondIndex = 0;
+	const returnArr = [];
+	let firstIndex = 0;
+	let secondIndex = 0;
 
-  while (firstIndex < arr1.length && secondIndex < arr2.length) {
-    debugger;
-    if (arr1[firstIndex] < arr2[secondIndex]) {
-      returnArr.push(arr1[firstIndex]);
-      firstIndex++;
-    } else {
-      returnArr.push(arr2[secondIndex]);
-      secondIndex++;
-    }
-  }
-  return returnArr;
+	while (firstIndex < arr1.length && secondIndex < arr2.length) {
+		if (arr1[firstIndex] < arr2[secondIndex]) {
+			returnArr.push(arr1[firstIndex]);
+			firstIndex++;
+		} else {
+			returnArr.push(arr2[secondIndex]);
+			secondIndex++;
+		}
+	}
+	return returnArr.concat(
+		arr1.slice(firstIndex).concat(arr2.slice(secondIndex)),
+	);
 }
