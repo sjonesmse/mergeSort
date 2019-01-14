e8d8a9a915c05f959807c744a61d24cc8ac65c6a
Sandy Jones <sjonesmse@gmail.com>
2019-01-14 14:07:22 -0600

    Sandy 1

--- mergesort.js
+++ mergesort.js
@@ -1,8 +1,27 @@
 function split(wholeArray) {
   /* your code here to define the firstHalf and secondHalf arrays */
 
-  let firstHalf;
-  let secondHalf;
+  let midPoint = Math.floor(wholeArray.length / 2);
+  let firstHalf = wholeArray.slice(0, midPoint);
+  let secondHalf = wholeArray.slice(midPoint);
 
   return [firstHalf, secondHalf];
+}
+
+function merge(arr1, arr2) {
+  let returnArr = [];
+  let firstIndex = 0;
+  let secondIndex = 0;
+
+  while (firstIndex < arr1.length && secondIndex < arr2.length) {
+    debugger;
+    if (arr1[firstIndex] < arr2[secondIndex]) {
+      returnArr.push(arr1[firstIndex]);
+      firstIndex++;
+    } else {
+      returnArr.push(arr2[secondIndex]);
+      secondIndex++;
+    }
+  }
+  return returnArr;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,11 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    // your code here
+    expect(split([2, 7, 4, 5, 7, 8])).toEqual([[2, 7, 4], [5, 7, 8]]);
+  });
+});
+
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    expect(merge([2, 7, 4], [5, 7, 8])).toEqual([2, 4, 5, 7, 7, 8]);
   });
 });
